f4af0274b2effb0ac4786a15dadcb01019326307
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-02-02 19:28:39 -0700

    setting up javascript outline

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+// function: gather user input from search button
+
+// function: input year output movies
+
+// function: input year output music
+
+// function: save local data (saving the year that was gathered from user input)
+
+// function: persist local data (reload page each refresh to display the saved data on screen)
